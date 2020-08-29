95d938964bb6e392367c9248b4cac92f4bad5178
Gurpreet Singh <GurpreetG.Singh@yahoo.com>
2020-08-29 23:19:39 +0530

    Update Main.java

--- src/com/gurpreet/Main.java
+++ src/com/gurpreet/Main.java
@@ -1,9 +1,21 @@
 package com.gurpreet;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Gurpreet's Java Programming.");
+
+        String[] values = new String[args.length];
+        int i=0;
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext())
+            values[i] = sc.next();
+            i++;
+        }
+
+
     }
+
 }
