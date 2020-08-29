29de95171d09fab074ac66040e3e2eabeda207e5
Gurpreet Singh <GurpreetG.Singh@yahoo.com>
2020-08-29 23:23:03 +0530

    Update Main.java

--- src/com/gurpreet/Main.java
+++ src/com/gurpreet/Main.java
@@ -5,17 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-
-        String[] values = new String[args.length];
-        int i=0;
-        Scanner sc = new Scanner(System.in);
-        while(sc.hasNext())
-            values[i] = sc.next();
-            i++;
-        }
-
-
+	System.out.print("Lets Begin!");
     }
 
 }
